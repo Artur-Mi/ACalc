cce1d980bd5f648c8a9a8c6217feef027585ab51
MisterArtKun <arturmiguel@live.com>
2022-09-17 17:44:22 -0300

    Versão (0.0.2) - Cálculo de Potências adicionando

--- src/main/java/misterartkun/acalc/ACalc.java
+++ src/main/java/misterartkun/acalc/ACalc.java
@@ -18,22 +18,23 @@ import java.util.Scanner;
  * @author Artur Miguel
  */
 public class ACalc {
-    static void calcula(int sw){
+    static void calcula(int sw) {// Método responsável pelas operações básicas
         int i=0;
         float resultado=0;
         do {
+            System.out.println("\n----------------------------------------------");
             System.out.println("\nDigite um valor: ");
             Scanner input = new Scanner(System.in);
-            if (i == 0){
+            if (i == 0) {
                 resultado = input.nextFloat();
                 i++;
             }
             else {
-                switch (sw){
-                    case 1 -> resultado = resultado + input.nextFloat();
-                    case 2 -> resultado = resultado - input.nextFloat();
-                    case 3 -> resultado = resultado / input.nextFloat();
-                    case 4 -> resultado = resultado * input.nextFloat();
+                switch (sw) {
+                    case 2 -> resultado = resultado + input.nextFloat();
+                    case 3 -> resultado = resultado - input.nextFloat();
+                    case 4 -> resultado = resultado / input.nextFloat();
+                    case 5 -> resultado = resultado * input.nextFloat();
                 }
                 
                 System.out.println("----------------------------------------------");
@@ -42,6 +43,45 @@ public class ACalc {
                 i = input.nextInt();
             }
         } while (i != 0);
+    }
+    
+    static void calculaPot(int sw) {
+        float resultado;
+        Scanner input = new Scanner(System.in);
+        if (sw == 2) {
+            System.out.println("Digite um valor: ");
+            resultado = input.nextFloat();
+            resultado = resultado * resultado;
+        }
+        else {
+            float x; 
+            int y;
+            System.out.println("Para o cálculo de exponenciação livre, é necessário fornecer o valor de X e Y: (Sendo X a base e Y o expoente)");
+            System.out.println("Digite o valor de X: ");
+            x = input.nextInt();
+            System.out.println("Digite o valor de Y: ");
+            y = input.nextInt();
+            if (y == 0) {
+                resultado = 1;
+            }
+            else if (y > 0) {
+                resultado = x;
+                while (y != 1) {
+                    resultado = resultado * x;
+                    y--;
+                }
+            }
+            else {
+                resultado = x;
+                while (y != -1) {
+                    resultado = resultado * x;
+                    y++;
+                }
+                resultado = 1 / resultado;
+            }
+        }
+        System.out.println("\n----------------------------------------------");
+        System.out.printf("Resultado: %f\n", resultado);
     }
 
     public static void main(String[] args) {
@@ -52,10 +92,8 @@ public class ACalc {
         do {
             System.out.println("----------------------------------------------");
             System.out.println("\nOpções: \n");
-            System.out.println("1 - Somar");
-            System.out.println("2 - Subtrair");
-            System.out.println("3 - Dividir");
-            System.out.println("4 - Multiplicar");
+            System.out.println("1 - Operações Básicas");
+            System.out.println("2 - Potência");
             System.out.println("0 - Sair");
             System.out.println("\nO que deseja realizar?");
         
@@ -63,14 +101,60 @@ public class ACalc {
             
             sw = input.nextInt();
             
-            if (sw == 0){
+            if (sw == 0) {
                 System.out.println("\nObrigado por usar o meu programa! :D");
             }
-            else if (sw > 4 || sw < 0) {
-                System.out.println("Insira um número dentro do escopo! (1 - 4)");
+            else if (sw > 2 || sw < 0) {
+                System.out.println("\n----------------------------------------------");
+                System.out.print("Insira um número dentro do escopo! (0 - 4)\n");
             }
             else {
-                calcula(sw);// É chamado o método calcula para que o cálculo seja efetuado
+                switch (sw) {
+                    case 1 -> {
+                        System.out.println("----------------------------------------------");
+                        System.out.println("\nOpções: \n");
+                        System.out.println("2 - Somar");
+                        System.out.println("3 - Subtrair");
+                        System.out.println("4 - Dividir");
+                        System.out.println("5 - Multiplicar");
+                        System.out.println("1 - Voltar");
+                        System.out.println("\nO que deseja realizar?");
+                        sw = input.nextInt();
+                        if (sw == 1) {
+                            break;
+                        }
+                        else if (sw > 5 || sw < 1) {
+                            System.out.println("\n----------------------------------------------");
+                            System.out.print("Insira um número dentro do escopo! (1 - 5)\n");
+                        }
+                        else {
+                            calcula(sw);// É chamado o método calcula para que o cálculo seja efetuado
+                        }
+                    }
+                    case 2 -> {
+                        System.out.println("----------------------------------------------");
+                        System.out.println("\nOpções: \n");
+                        System.out.println("2 - Quadrado");
+                        System.out.println("3 - Exponenciação");
+                        System.out.println("1 - Voltar");
+                        System.out.println("\nO que deseja realizar?");
+                        sw = input.nextInt();
+                        if (sw == 1) {
+                            break;
+                        }
+                        else if (sw > 3 || sw < 1) {
+                            System.out.println("\n----------------------------------------------");
+                            System.out.print("Insira um número dentro do escopo! (1 - 5)\n");
+                        }
+                        else {
+                            do {
+                                calculaPot(sw);
+                                System.out.println("\nDeseja continuar calculando potências?(2 - Quadrados, 3 - E.Livre, 1 - NÃO");
+                                sw = input.nextInt();
+                            } while (sw != 1);
+                        }
+                    }
+                }
             }            
         } while(sw != 0);
     }
